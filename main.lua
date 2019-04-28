171fd01a612c02e207f458cbe6494a20c3c043cd
Colby Leiske <colby.leiske@gmail.com>
2019-04-28 00:39:12 -0700

    latest graphical update

--- main.lua
+++ main.lua
@@ -1,8 +1,12 @@
 Gamestate = require "lib.hump.gamestate"
 logger = require "src.util.logger"
-
+Sprites = require 'src.util.spriteloader'
 function love.load()
     logger:log("Starting up", logger.MISC)
+
+    Sprites:loadSprites()
+
+
     leaderboard = require "src.states.leaderboard"
     game = require "src.states.game"
 
--- src/entities/grid.lua
+++ src/entities/grid.lua
@@ -59,6 +59,8 @@ function Grid:update(dt)
 end
 
 function Grid:tick()
+	self:checkForCompletedLines()
+
 	if self.playerBlock then 
 		didMove = self:movePlayerBlockDown() 
 	end
@@ -66,24 +68,24 @@ function Grid:tick()
 		self:placePlayerBlock()
 	end
 
-	self:checkForCompletedLines()
+	--self:fixStraglers()
 end
 
 function Grid:draw()
 	love.graphics.scale(Constants.windowScaleFactor,Constants.windowScaleFactor)
 
-	for col=1, Constants.gridWidth do
-		love.graphics.line(Constants.tileWidth*col, 0, Constants.tileWidth*col, love.graphics.getHeight())
-	end
+	-- for col=1, Constants.gridWidth do
+	-- 	love.graphics.line(Constants.tileWidth*col, 0, Constants.tileWidth*col, love.graphics.getHeight())
+	-- end
 
-	for row=1, Constants.gridHeight do
-		love.graphics.line(0, Constants.tileHeight*row, love.graphics.getWidth(), Constants.tileHeight*row)
-	end
+	-- for row=1, Constants.gridHeight do
+	-- 	love.graphics.line(0, Constants.tileHeight*row, love.graphics.getWidth(), Constants.tileHeight*row)
+	-- end
 
 	for j=1 , Constants.gridHeight do
 		for i=1, Constants.gridWidth do
 			if self.grid[j][i].occupied then
-				love.graphics.rectangle("fill",(i-1)*Constants.tileWidth,(j-1-Constants.gridHeightBuffer)*Constants.tileHeight,Constants.tileWidth,Constants.tileHeight)
+				love.graphics.draw(self.grid[j][i].BlockType.blockSprite,(i-1)*Constants.tileWidth,(j-1-Constants.gridHeightBuffer)*Constants.tileHeight)
 			end
 		end
 	end
@@ -91,7 +93,7 @@ end
 
 function Grid:newPlayerBlock() 
 	self.playerBlock = PlayerBlock(Vector(Constants.gridWidth/2,1),Blocks.Straight)
-	for k,v in pairs(self.playerBlock.blockType) do
+	for k,v in pairs(self.playerBlock.blockType.blocks) do
 		blockPos = v + self.playerBlock.origin
 		self.grid[blockPos.y][blockPos.x] = {occupied = true, BlockType = self.playerBlock.blockType, isPlayerBlock = true, newlyPlaced = false}
 	end
@@ -101,23 +103,23 @@ end
 function Grid:movePlayerBlockDown()
 	origin = self.playerBlock.origin
 	placesToCheck = {}
-	for k,v in pairs(self.playerBlock.blockType) do
+	for k,v in pairs(self.playerBlock.blockType.blocks) do
 		v = origin + v + Vector(0,1)
 		if v.y > Constants.gridHeight then return false end
 		if self.grid[v.y][v.x].occupied == true and self.grid[v.y][v.x].isPlayerBlock == false then return false end
 		
 	end
 	--breaks when i move it up to the top for loop lol
-	for k,v in pairs(self.playerBlock.blockType) do
+	for k,v in pairs(self.playerBlock.blockType.blocks) do
 		blockPos = origin + v
 		if self.grid[blockPos.y][blockPos.x].newlyPlaced == false then
 			self.grid[blockPos.y][blockPos.x] = {occupied=false,BlockType = Blocks.None,isPlayerBlock = false,newlyPlaced = false}
 		end
-		self.grid[blockPos.y+1][blockPos.x] = {occupied=true,self.playerBlock.blockType, isPlayerBlock = true, newlyPlaced = true}
+		self.grid[blockPos.y+1][blockPos.x] = {occupied=true,BlockType = self.playerBlock.blockType, isPlayerBlock = true, newlyPlaced = true}
 	end
 
 	self.playerBlock.origin = self.playerBlock.origin + Vector(0,1)
-	for k,v in pairs(self.playerBlock.blockType) do
+	for k,v in pairs(self.playerBlock.blockType.blocks) do
 		blockPos = self.playerBlock.origin + v
 		self.grid[blockPos.y][blockPos.x].newlyPlaced = false
 	end
@@ -126,33 +128,33 @@ end
 function Grid:movePlayerBlockXAxis(movementAxis)
 	origin = self.playerBlock.origin
 	placesToCheck = {}
-	for k,v in pairs(self.playerBlock.blockType) do
+	for k,v in pairs(self.playerBlock.blockType.blocks) do
 		v = origin + v + movementAxis
 		if v.x > Constants.gridWidth or v.x <= 0 then return false end
 		if self.grid[v.y][v.x].occupied == true and self.grid[v.y][v.x].isPlayerBlock == false then return false end
 		
 	end
 	--breaks when i move it up to the top for loop lol
-	for k,v in pairs(self.playerBlock.blockType) do
+	for k,v in pairs(self.playerBlock.blockType.blocks) do
 		blockPos = origin + v
 		if self.grid[blockPos.y][blockPos.x].newlyPlaced == false then
 			self.grid[blockPos.y][blockPos.x] = {occupied=false,BlockType = Blocks.None,isPlayerBlock = false,newlyPlaced = false}
 		end
-		self.grid[blockPos.y][blockPos.x + movementAxis.x] = {occupied=true,self.playerBlock.blockType, isPlayerBlock = true, newlyPlaced = true}
+		self.grid[blockPos.y][blockPos.x + movementAxis.x] = {occupied=true,BlockType = self.playerBlock.blockType, isPlayerBlock = true, newlyPlaced = true}
 	end
 
 	self.playerBlock.origin = self.playerBlock.origin + movementAxis
-	for k,v in pairs(self.playerBlock.blockType) do
+	for k,v in pairs(self.playerBlock.blockType.blocks) do
 		blockPos = self.playerBlock.origin + v
 		self.grid[blockPos.y][blockPos.x].newlyPlaced = false
 	end
 end
 
 function Grid:placePlayerBlock()
-	for k,v in pairs(self.playerBlock.blockType) do
+	for k,v in pairs(self.playerBlock.blockType.blocks) do
 		v = origin + v
 		self.grid[v.y][v.x].isPlayerBlock = false
-		self.grid[v.y][v.x].blockType = self.playerBlock.blockType		
+		self.grid[v.y][v.x].BlockType = self.playerBlock.blockType		
 	end
 	self:newPlayerBlock()
 end
@@ -186,6 +188,41 @@ function Grid:checkForCompletedLines()
 			print("woohoo line cleared " .. v)
 		end
 	end
+end
+
+function Grid:fixStraglers() 
+	surroundingBlocks = 0
+	for i=1,Constants.gridHeight do
+		for j=1,Constants.gridWidth do
+			if self.grid[i][j].isPlayerBlock == true then 
+				if i-1 > 0 then
+					if self.grid[i-1][j].occupied == false then surroundingBlocks = surroundingBlocks + 1 end
+					if j-1 > 0 then if self.grid[i-1][j-1].occupied == false then surroundingBlocks = surroundingBlocks + 1 end end
+					if j+1 <= Constants.gridHeight then if self.grid[i-1][j+1].occupied == false then surroundingBlocks = surroundingBlocks + 1 end end
+				end
+				if i+1 <= Constants.gridWidth then
+					if self.grid[i+1][j].occupied == false then surroundingBlocks = surroundingBlocks + 1 end
+					if j-1 > 0 then if self.grid[i+1][j-1].occupied == false then surroundingBlocks = surroundingBlocks + 1 end end
+					if j+1 <= Constants.gridHeight then if self.grid[i+1][j+1].occupied == false then surroundingBlocks = surroundingBlocks + 1 end end
+				end
+				if j-1 > 0 then
+					if self.grid[i][j-1].occupied == false then surroundingBlocks = surroundingBlocks + 1 end
+					if i-1 > 0 then if self.grid[i-1][j-1].occupied == false then surroundingBlocks = surroundingBlocks + 1 end end
+					if i+1 <= Constants.gridWidth then if self.grid[i+1][j-1].occupied == false then surroundingBlocks = surroundingBlocks + 1 end end
+				end
+				if j+1 <= Constants.gridHeight then
+					if self.grid[i][j+1].occupied == false then surroundingBlocks = surroundingBlocks + 1 end
+					if i-1 > 0 then if self.grid[i-1][j+1].occupied == false then surroundingBlocks = surroundingBlocks + 1 end end
+					if i+1 <= Constants.gridWidth then if self.grid[i+1][j+1].occupied == false then surroundingBlocks = surroundingBlocks + 1 end end
+				end
+			 end
+		end
+	end
+
+	if surroundingBlocks == 0 then
+		print('potential floater found')
+	end
+
 end
 
 return Grid
